9d23fa63002da1ba3d08723fff69e9e656e9bc64
cristian-m-barrios <cristianmartinbarrios1993@gmail.com>
2025-03-25 00:49:07 -0300

    Add files via upload
    Este programa permite saber si un numero ingresado es primo.

---
+++ NUMERO PRIMO INGRESADO.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int main() {
+	int num1,contador,i ;
+	printf("Ingrese un numero para saber si es primo \n");
+	scanf("%d",&num1);
+	contador = 0;
+	for(i=2;i < num1; i++)
+	{
+		if (num1 %i == 0)
+		{
+		contador++;	
+		}
+	}
+	if (contador == 0)
+	{
+		printf("El numero %d",num1);
+		printf(" es primo");
+	}
+	else
+	{
+		printf("El numero no es primo");
+	}
+	
+	return 0;
+}
